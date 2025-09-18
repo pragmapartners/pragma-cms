a8327a5801c7bba0b36f92ab742c5e6468eab821
Brynn Briedis <brynn@pragma.com.au>
2025-09-18 16:04:29 +1000

    update

---
+++ src/components/Payload/Icon.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import Mark from '@/components/Icons/Mark';
+
+export default function Icon() {
+  return <div className='dark:text-white'><Mark className='w-full' /></div>;
+}
---
+++ src/components/Payload/Logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Wordmark from '@/components/Icons/Logo';
+
+export default function Logo() {
+  return (
+    <div className='dark:text-white'>
+      <Wordmark className='w-full' />
+    </div>
+  );
+}
